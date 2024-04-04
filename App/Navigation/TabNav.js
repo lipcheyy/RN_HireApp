4b4921d34c795c401435a88c88499ca501567685
Vladislav <vladlipchey05@gmail.com>
2024-04-04 20:47:38 +0300

    add bottom nav styles

--- App/Navigation/TabNav.js
+++ App/Navigation/TabNav.js
@@ -1,22 +1,59 @@
-import { View,Text } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from "../Screens/HomeScreen/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import ProfileScreen from "../Screens/ProfileScreen/ProfileScreen";
 import BookingScreen from "../Screens/BookingScreen/BookingScreen";
-const Tab=createBottomTabNavigator();
 
-export default function TabNavigation(){
-    return(
+import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Colors from "../Utils/Colors";
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigation() {
+    return (
         <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name="home" component={HomeScreen}/>
-            
-            <Tab.Screen name="booking" component={BookingScreen}/>
-            
-            <Tab.Screen name="profile" component={ProfileScreen}/>
-        </Tab.Navigator>
+            <Tab.Navigator screenOptions={
+                { headerShown: false,
+                  tabBarActiveTintColor:Colors.PRIMARY }
+            }>
+                <Tab.Screen name="home" component={HomeScreen}
+                    options={{
+                        tabBarLabel: ({ color }) => (
+                            <Text style={{ color: color, fontSize: 12, marginTop: -7 }}>Home</Text>
+                        ),
+                        tabBarIcon: ({color,size})=>(
+                            <FontAwesome5 name="home" size={size} color={color} />
+                        )
+                }}
+                
+            />
+
+                <Tab.Screen name="booking" component={BookingScreen} 
+                    options={{
+                        tabBarLabel: ({ color }) => (
+                            <Text style={{ color: color, fontSize: 12, marginTop: -7 }}>Booking</Text>
+                        ),
+                        tabBarIcon: ({color,size})=>(
+                            <FontAwesome name="list-ul" size={size} color={color} />
+                        )
+                }}
+                />
+
+                <Tab.Screen name="profile" component={ProfileScreen} 
+                    options={{
+                        tabBarLabel: ({ color }) => (
+                            <Text style={{ color: color, fontSize: 12, marginTop: -7 }}>Booking</Text>
+                        ),
+                        tabBarIcon: ({color,size})=>(
+                            <FontAwesome name="user" size={size} color={color} />
+                        )
+                }}
+                />
+            </Tab.Navigator>
         </NavigationContainer>
     )
 }
--- App/Screens/HomeScreen/HomeScreen.jsx
+++ App/Screens/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
     return (
-        <View>
+        <SafeAreaView>
             <Text>sui</Text>
-        </View>
+        </SafeAreaView>
     )
   
 }
